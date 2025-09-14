c2ad829a6f861c9f3d06bb7732113f2b6c41971e
Elliot Kolker-Hicks <ekolkerh@charlotte.edu>
2025-09-14 12:51:10 -0400

    Added D3JS chart

---
+++ public/oldChart.js
@@ -0,0 +1,39 @@
+var dataSource = {
+                datasets: [
+                    {
+                        data: [],
+                        backgroundColor: [
+                            '#ffcd56',
+                            '#ff6384',
+                            '#36a2eb',
+                            '#fd6b19',
+                            '#fd6b19',
+                            '#020887',
+                            '7be0ad'
+
+                        ]
+                    }
+                ],
+                labels: []
+            };
+
+        function createChart() {
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myPieChart = new Chart(ctx, {
+                type: 'pie',
+                data: dataSource
+            });
+        }
+
+        function getBudget() {
+            axios.get('http://localhost:3000/budget')
+            .then(function (res) {
+                for (var i = 0; i < res.data.myBudget.length; i++) {
+                    dataSource.datasets[0].data[i] = res.data.myBudget[i].budget;
+                    dataSource.labels[i] = res.data.myBudget[i].title;
+                    createChart();
+                }
+            });
+        }
+
+        getBudget();
